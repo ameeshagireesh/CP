f535208e90be6978d0a9a2a5829c608088fe35af
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-11-07 20:11:52 +0400

    feat: added solution of two sum

---
+++ LeetCode/Easy/1. Two Sum.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == nums[j]+nums[i]:
+                    return [i,j]
